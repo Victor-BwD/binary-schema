5143214c48f1a689002cb5afb48ef3b756c876be
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2025-09-25 23:05:20 -0300

    Column type

--- main.go
+++ main.go
@@ -90,12 +90,9 @@ func (c *Column) Binary() []uint8 {
 func PutUvarint(slicedArray []byte, x uint64) []byte {
 	if x == 0 {
 		slicedArray[0] = 0xc0
-		fmt.Printf("0x%02x ", slicedArray[0])
 		return slicedArray[:1]
 	}
-
 	indice := 1
-
 	for j := 56; j >= 0; j -= 8 {
 		b := byte(x >> j)
 		if b != 0 || j == 0 {
@@ -103,57 +100,20 @@ func PutUvarint(slicedArray []byte, x uint64) []byte {
 			indice++
 		}
 	}
-
 	size := indice - 1
 	slicedArray[0] = 0xc0 | byte(size)
-	for i := 0; i < indice; i++ {
-		fmt.Printf("0x%02x ", slicedArray[i])
-	}
-
-	/*if x <= 0xff {
-		slicedArray[0] = byte(x)
-		fmt.Printf("0b%08b\n", slicedArray[0])
-		return 1
-	}
-	if x <= 0xffff {
-		slicedArray[0] = byte(x)
-		slicedArray[1] = byte(x >> 8)
-		fmt.Printf("0b%08b, 0b%08b\n", slicedArray[0], slicedArray[1])
-		return 2
-	}
-	if x <= 0xffffffff {
-		slicedArray := make([]byte, 5)
-
-		slicedArray[0] = 0xc0 | 4
-		slicedArray[1] = byte(x >> 24)
-		slicedArray[2] = byte(x >> 16)
-		slicedArray[3] = byte(x >> 8)
-		slicedArray[4] = byte(x)
-		fmt.Printf("0b%08b, 0b%08b, 0b%08b, 0b%08b, 0b%08b\n",
-			slicedArray[0], slicedArray[1], slicedArray[2], slicedArray[3], slicedArray[4])
-		return 4
-	}*/
-
 	return slicedArray[:indice]
 }
 
 func PutString(buffer []byte, str string) int {
 	length := byte(len(str))
 
-	fmt.Printf("Tamanho da string: %d\n", byte(length))
-
 	buffer[0] = 0xd1 // 0b11010001, indicando que é uma string
-	fmt.Printf("0x%02x ", buffer[0])
 
 	lengthBytes := PutUvarint(buffer[1:], uint64(length)) // Passa a string e o tamanho dela para a função trazer o tamanho em bytes
 	bytesWritten := len(lengthBytes)
 
 	copy(buffer[1+bytesWritten:], []byte(str)) // copia a string para o buffer, começando após o indice de bytes escritos
-
-	strStart := 1 + bytesWritten // Calcula o índice onde a string começa no buffer
-	for i := strStart; i < strStart+len(str); i++ {
-		fmt.Printf("0x%02x ", buffer[i])
-	}
 
 	return 1 + bytesWritten + len(str)
 }
@@ -168,6 +128,18 @@ func PutColumnType(buffer []byte, columnType ColumnType, nullable bool) (int, er
 	buffer[1] = value
 	fmt.Printf("0x%02x 0x%02x ", buffer[0], buffer[1])
 
+	return 2, nil
+}
+
+func PutColumnType(buffer []byte, columnType ColumnType, nullable bool) (int, error) {
+	value, err := columnType.Binary(nullable)
+	if err != nil {
+		return 0, err
+	}
+	buffer[0] = 0xd2
+	buffer[1] = value
+	fmt.Printf("0x%02x 0x%02x ", buffer[0], buffer[1])
+	
 	return 2, nil
 }
 
